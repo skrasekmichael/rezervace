fcd51a4c622ecbfcbe985ff4e8f6a35e7fe7d202
skrasekmichael <skrasek.michal@email.cz>
2018-05-23 23:34:04 +0200

    návrhy na zlepšení

--- controllers/tools_controller.php
+++ controllers/tools_controller.php
@@ -10,7 +10,11 @@ class tools_controller extends controller
     //uvidime co z toho bude, chtel bych aby se nacetla value z tlacitka v dalsi
     //navigaci ohledne spravy a pak ve switchem se udelala spravna tabulka
     //zkusim to udelat do patku, a kdyz ne, tak ti reknu
-        switch($choice){
+        
+    
+    //v kontroleru nesmíš vypisovat ... uložíš to do $this->data["jméno proměnný"] a to pak vypíšeš v pohledu ;)
+    /*
+    switch($choice){
             case "Události":
                 $data = ChooseTable::loadEvents;
                 if($data[0]){
@@ -35,10 +39,16 @@ class tools_controller extends controller
                         echo "<tr>$row[5]</tr>";
                     }
                 }
-         }       
-    }
-}
-?>
-    
+        }       */
+        
+        //př. jak bych to ufělal
+        $list_of_users = "<table>";
+        $users = Db::query_all("SELECT * FROM user");
+        for ($i = 0; $i < count($users); $i++)
+        {
+            $list_of_users .= "<tr><td>" . $users[$i]["firstname"] . "</td><td>" . $users[$i]["lastname"] .  "</td><td>funkce 1</td><td>funnkce 2 ...</tr>";
+        }
+        $list_of_users .= "</table>";
+        $this->data["users"] = $list_of_users;
     }
 }
